1febbc3cd5d8f5f8f81c25edbd5ee1274859c3c8
Nithiesh Mohan <nithiesh164@gmail.com>
2024-01-29 17:50:10 +0530

    Added Postgres.

--- FlashCards/Program.cs
+++ FlashCards/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace FlashCards;
+﻿using Npgsql;
+
+namespace FlashCards;
 
 class Program
 {
     static void Main(string[] args)
     {
+        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=Aedwasd*2001;Database=postgres";
+        NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        
+        connection.Open();
+
+        var tablecmd = connection.CreateCommand();
+        tablecmd.CommandText = @"CREATE TABLE IF NOT EXISTS flashcards (
+    Id SERIAL PRIMARY KEY,
+    Date TIMESTAMP
+)";
+        tablecmd.ExecuteNonQuery();
+        connection.Close();
         Menu.MenuSelector();
     }
 }
