f5f8508696e4b0af26aa4e3599e9d92d08e3ddad
Nithiesh Mohan <nithiesh164@gmail.com>
2024-01-31 22:42:01 +0530

    .

--- FlashCards/Menu.cs
+++ FlashCards/Menu.cs
@@ -31,7 +31,7 @@ public class Menu
                 }
                 case "1":
                 {
-                    //add();
+                    add();
                     break;
                 }
                 case "2":
